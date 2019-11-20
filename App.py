8350f895421cb17673ff1d71c8b5a38d01e5e46b
Mackenzie <guest173800@gmail.com>
2019-11-20 14:06:46 -0500

    Update App.py

--- App.py
+++ App.py
@@ -1,36 +1,22 @@
-import numpy as np
 from sklearn.neighbors import KNeighborsClassifier
-import cv2
-import os
-import random
+import pickle
+import numpy as np
 import pyautogui
-import time
+import cv2
+import Train
 
 ANSWERS = ['R', 'G', 'B', 'L', 'D', 'H', 'J']
+model = None
 
-data = os.listdir("data")
-random.shuffle(data)
+def load_model():
+    global model
+    try:
+        model = pickle.load(open('model.pickle', 'rb'))
+    except FileNotFoundError:
+        print('Model not found, creating a new one...')
+        model = Train.generate_model()
 
-training_data = []
-training_answer = []
-
-for file in data:
-    training_data.append(cv2.imread('data/' + file, cv2.IMREAD_GRAYSCALE))
-    training_answer.append(ANSWERS.index(file[0]))
-
-training_data = np.array(training_data)
-nsamples, nx, ny = training_data.shape
-training_data = training_data.reshape((nsamples,nx*ny))
-
-training_data, testing_data = np.array_split(training_data, 2)
-training_answer, testing_answer = np.array_split(training_answer, 2)
-
-clf = KNeighborsClassifier()
-clf.fit(training_data, training_answer)
-
-print('Score: ', clf.score(testing_data, testing_answer))
-
-while True:
+def main():
     image = pyautogui.screenshot()
     image = np.array(image)
     x = 733
@@ -47,12 +33,13 @@ while True:
             dx = x + 75
             dy = y + 75
             
-            test_image = image[y:dy, x:dx]
-            test_image = np.array(test_image)
-            mnx, mny = test_image.shape
-            test_image = test_image.reshape((mnx*mny))
+            orb_image = image[y:dy, x:dx]
+            orb_image = cv2.cvtColor(orb_image, cv2.COLOR_BGR2GRAY)
+            orb_image = np.array(orb_image)
+            mnx, mny = orb_image.shape
+            orb_image = orb_image.reshape((mnx*mny))
             
-            prediction = ANSWERS[clf.predict([test_image])[0]]
+            prediction = ANSWERS[model.predict([orb_image])[0]]
             image = cv2.putText(image, prediction, (x + 38,y + 38), cv2.FONT_HERSHEY_PLAIN, 1.2, (0, 0, 0))
             image = cv2.putText(image, prediction, (x + 37,y + 37), cv2.FONT_HERSHEY_PLAIN, 1.2, (row * 40, column * 40, 255))
             image = cv2.rectangle(image, (x, y), (dx, dy), (255, 255, 255), 1)
@@ -60,3 +47,8 @@ while True:
     
     cv2.imshow("Screenshot", image)
     cv2.waitKey(100)
+
+if __name__ == "__main__":
+    load_model()
+    while True:
+        main()
